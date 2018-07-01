dd61491f40320655fa94ce2595a2ae6bd038d48a
Viktor <v.melnychuk12@gmail.com>
2018-07-01 21:46:24 +0300

    initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TransactionsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
