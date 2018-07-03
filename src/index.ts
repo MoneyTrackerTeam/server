c9fed24a0b1e3922d8db7ff239e205066eeafe4b
Viktor <v.melnychuk12@gmail.com>
2018-07-03 15:20:21 +0300

    logs on backend

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import { DatabaseProvider } from "./database";
+import { DatabaseProvider, MongoProvider } from "./database";
 import { ApiServer } from "./server";
 const port = +process.env.PORT || 3000;
 
@@ -10,6 +10,10 @@ DatabaseProvider.configure({
     port: 5432,
     username: "money",
     password: "123",
+});
+
+MongoProvider.configure({
+    url: "mongodb://localhost:27017/logs",
 });
 
 const server = new ApiServer();
